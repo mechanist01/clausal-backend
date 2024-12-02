ab3c10e660ebc0545d7a2ce48a3f81a76dd7b5cc
mechanist01 <120581886+mechanist01@users.noreply.github.com>
2024-12-02 17:22:27 -0500

    replit

--- replit.nix
+++ replit.nix
@@ -1,28 +1,31 @@
 { pkgs }: {
     deps = [
-        pkgs.python310Full
-        pkgs.pip
-        pkgs.replitPackages.prybar-python310
-        pkgs.replitPackages.stderred
-        # PDF dependencies
-        pkgs.mupdf
-        pkgs.ghostscript
-        pkgs.freetype
-        pkgs.zlib
-        # System libraries
-        pkgs.glib
-        pkgs.cairo
-        pkgs.pango
-        pkgs.gdk-pixbuf
-        # Build tools
-        pkgs.pkg-config
-        pkgs.gcc
+      pkgs.rustc
+      pkgs.libiconv
+      pkgs.cargo
+      pkgs.python310Full
+      pkgs.pip
+      pkgs.replitPackages.prybar-python3
+      pkgs.replitPackages.stderred
+      # PDF dependencies
+      pkgs.mupdf
+      pkgs.ghostscript
+      pkgs.freetype
+      pkgs.zlib
+      # System libraries
+      pkgs.glib
+      pkgs.cairo
+      pkgs.pango
+      pkgs.gdk-pixbuf
+      # Build tools
+      pkgs.pkg-config
+      pkgs.gcc
     ];
     env = {
         PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
         LANG = "en_US.UTF-8";
         STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-        PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
+        PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python3}/bin/prybar-python3";
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
             pkgs.mupdf
             pkgs.ghostscript
