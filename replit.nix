67f0689184a88373048d4a69de305351df71ed20
mechanist01 <120581886+mechanist01@users.noreply.github.com>
2024-12-02 17:36:58 -0500

    nix

--- replit.nix
+++ replit.nix
@@ -3,9 +3,7 @@
       pkgs.rustc
       pkgs.libiconv
       pkgs.cargo
-      pkgs.python3
-      pkgs.python3Packages.pip  # Changed from pkgs.pip
-      pkgs.replitPackages.stderred
+      pkgs.python3Full
       # PDF dependencies
       pkgs.mupdf
       pkgs.ghostscript
@@ -21,9 +19,8 @@
       pkgs.gcc
     ];
     env = {
-        PYTHONBIN = "${pkgs.python3}/bin/python3";
+        PYTHONBIN = "${pkgs.python3Full}/bin/python3";
         LANG = "en_US.UTF-8";
-        STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
             pkgs.mupdf
             pkgs.ghostscript
